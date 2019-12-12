8ad0c5d50903f67544ab514634699756536ac2d8
Leban <pablo.leban@yrbrands.com>
2019-12-12 14:14:59 -0300

    Progress

--- tools.js
+++
@@ -1,7 +0,0 @@
-module.exports = class ImageFieldConnector {
-    constructor(inputSelector, imgSelector, context) {
-        context.find(inputSelector).on("input", () => {
-            imgSelector.attr("src", $(inputSelector).val())
-        })
-    }
-}
